c46773ee9eac307ffa5b60cd08658fe6324067ce
Edwin Schaap <edwin@schaap.me>
2016-08-04 21:54:55 +0200

    meh

--- app.js
+++ app.js
@@ -3,39 +3,73 @@ function start() {
 
   var site = 'https://github.com/edwinschaap/heist/raw/gh-pages/index.html';
 
-  var T1, T2minT1, interval;
+  fetchUrl(site, 4000);
 
-  function payload(length){
-    var r = '';
-    for (var i=length; i>0; --i) {
-      r += Math.random().toString(36).slice(2,3);
-    }
-    return r;
-  }
+  // var T1, T2, T2minT1, interval;
 
-  try {
-    fetch(site, {
-      mode: 'no-cors'
-    })
-    .then(function(result){
-      T1 = performance.now();
-      clearInterval(interval);
-    })
-    .catch(function(e){
-      console.log(e);
-    });
-  } catch (e) {
-    if (e instanceof TypeError) {
-      console.log(e);
-    }
-  }
+  //
+  // try {
+  //   fetch(site, {
+  //     mode: 'no-cors'
+  //   })
+  //   .then(function(result){
+  //     T1 = performance.now();
+  //     clearInterval(interval);
+  //   })
+  //   .catch(function(e){
+  //     console.log(e);
+  //   });
+  // } catch (e) {
+  //   if (e instanceof TypeError) {
+  //     console.log(e);
+  //   }
+  // }
+
+  // interval = setInterval(function(){
+  //   var entries = performance.getEntries();
+  //   var lastEntry = entries[entries.length-1];
+  //   if (lastEntry.name == site) {
+  //     T2minT1 = lastEntry.responseEnd - T1;
+  //     console.log(T2minT1);
+  //   }
+  // });
+}
+
+function fetchUrl(site, payloadSize) {
+  var T1, T2, interval;
+
+  site += '?'+ payload(payloadSize);
+  console.log('Fetching '+site);
 
   interval = setInterval(function(){
     var entries = performance.getEntries();
     var lastEntry = entries[entries.length-1];
     if (lastEntry.name == site) {
-      T2minT1 = lastEntry.responseEnd - T1;
-      console.log(T2minT1);
+      T2 = lastEntry.responseEnd;
+//      T2minT1 = T2 - T1;
+//      console.log(T2minT1);
+      console.log('T1: '+T1+', T2: '+T2);
     }
   });
+
+  fetch(site, {
+    mode: 'no-cors'
+  })
+  .then(function(result){
+    T1 = performance.now();
+    clearInterval(interval);
+  })
+  .catch(function(e){
+    console.log(e);
+  });
+
+
+}
+
+function payload(length){
+  var r = '';
+  for (var i=length; i>0; --i) {
+    r += Math.random().toString(36).slice(2,3);
+  }
+  return r;
 }
